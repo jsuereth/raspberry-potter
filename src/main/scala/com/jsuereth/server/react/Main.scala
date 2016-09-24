a026bb52d983b6657feaaa2d41633c4c0266dbf9
Josh Suereth <joshua.suereth@gmail.com>
2016-09-24 16:34:09 -0400

    Dump of dumb image generation algorithm.

--- src/main/scala/com/jsuereth/server/react/Main.scala
+++ src/main/scala/com/jsuereth/server/react/Main.scala
@@ -1,5 +1,9 @@
 package com.jsuereth.server.react
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
 import com.jsuereth.server.collections.CircularBuffer
 import com.jsuereth.server.ir.TrackedObjectUpdate
 import io.reactors.Events
@@ -20,6 +24,23 @@ object Main {
     tmp
   }
 
+  val historyImage = history map renderHistory
+
+  /** Draws the current history of tracked objects to an image by connecting points with lines. */
+  def renderHistory(buf: CircularBuffer[CameraUpdate]): BufferedImage = {
+    // TODO - Mutate an existing image rather than creating new memory each time?
+    val img = new BufferedImage(1024, 768, BufferedImage.TYPE_BYTE_GRAY)
+    val g = img.getGraphics
+    g.setColor(Color.BLACK)
+    // TODO - Maybe not use straight lines?
+    for {
+      (prev, next) <- buf.iterator zip (buf.iterator drop 1)
+      (pobj, nobj) <- prev.objects zip next.objects
+      if !(pobj.isEmpty || nobj.isEmpty)
+    } g.drawLine(pobj.x, pobj.y, nobj.x, nobj.y)
+    img
+  }
+
 
   def main(args: Array[String]): Unit = {
     I2CController.camera onEvent { objs =>
@@ -28,6 +49,10 @@ object Main {
         if !obj.isEmpty
       } yield s"$idx, $obj").mkString("\n")
       System.err.print(s"--== Update ==--\n$msg\n")
+    }
+    // Dump rendered image to a png file we can look at.
+    historyImage map { buf =>
+      ImageIO.write(buf, "png", new java.io.File("objects.png"))
     }
 
 
